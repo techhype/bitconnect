661247176fcb109c6207cad8dabb16a304e8aa5e
techhype <kishore.a.2016.cse@rajalakshmi.edu.in>
2020-08-18 16:38:07 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,80 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {useForm} from 'react-hook-form';
+import { StyleSheet, Text, TextInput, View, TouchableHighlight} from 'react-native';
+
+class signUpButton extends Component {
+  render(){
+    return (
+      <TouchableHighlight>
+          <View style={styles.button}>
+            <Text style={{color:'white',fontSize:14}}>Sign Up</Text>
+          </View>
+      </TouchableHighlight>
+    );
+  }  
+};
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Demo form</Text>
+      <View >
+        <TextInput placeholder="User Name" style={styles.inputStyle} />
+        <TextInput placeholder="Email" style={styles.inputStyle}/>
+        <TextInput placeholder="Password" style={styles.inputStyle} secureTextEntry={true} />
+        <TextInput placeholder="Mobile Number" style={styles.inputStyle}/>
+        <signUpButton />
+        <TouchableHighlight>
+          <View style={styles.button}>
+            <Text style={{color:'white',fontSize:14}}>Sign Up</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+  },
+  inputStyle: {
+    margin: 20,
+    width: 200,
+    height: 30,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
+  button:{
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#176CDC',
+    color:'#fff',
+    alignItems: "center",
+    backgroundColor: "#176CDC",
+    width:100,
+    marginLeft:60,
+    marginTop:10,
+    padding: 5, 
+    // Shadow  
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+  
+  
+});
